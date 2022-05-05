bb5b26bb8ff708a8c570862d421c47ffbe61e629
mreginato3 <marcos.reginato1995@gmail.com>
2022-05-05 11:53:57 -0300

    deco speedup sale order views

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "custom/repositories/customs/deco_sale_order",
+    'name': "deco_sale_order",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
---
+++ models/sale_order.py
@@ -0,0 +1,6 @@
+from odoo import api, fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+    _order = False
